6d4a8e78daf7d9d1d531994a28266cfa9836f98c
Jason Hoang <jhoang2@ualberta.ca>
2022-05-22 17:26:33 -0600

    Revert previous change

--- src/App.js
+++ src/App.js
@@ -4,21 +4,9 @@ import Intro from './components/Intro';
 import About from './components/About';
 import Projects from './components/Projects';
 import Credits from './components/Credits';
-import Loader from './components/Loader';
-import { useState, useEffect } from 'react';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
-
-  return loading ? (
-    <Loader></Loader>
-  ) : (
+  return (
     <div className="App">
       <Navbar></Navbar>
       <Intro></Intro>
