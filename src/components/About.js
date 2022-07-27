3a0ded560d72b4a99aa70551d475305120f2cbd0
Jason Hoang <jhoang2@ualberta.ca>
2022-07-26 19:24:22 -0600

    Change about section

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ function About() {
                 <p className='about-body'>
                     I am a fourth-year Computing Science major at the <a href="https://www.ualberta.ca/about/index.html">University of Alberta</a>.
                     My interest in programming encompasses anything from machine learning and artificial intelligence to web and game development,
-                    and I enjoy trying to learn new things. Some of my other hobbies include basketball, video games, reading, and chess.
+                    and I enjoy trying to learn new things. Some of my other hobbies include basketball, golf, video games, and chess.
                 </p>
             </div>
             <div id='about-picture'>
